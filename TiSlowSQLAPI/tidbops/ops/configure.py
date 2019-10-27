a4d4c905f7a44b61e8b0a31fed5309f685163a7b
linruichao <linruichao@autohome.com.cn>
2019-10-27 11:09:55 +0800

    add api

--- TiSlowSQLAPI/tidbops/ops/configure.py
+++ TiSlowSQLAPI/tidbops/ops/configure.py
@@ -67,26 +67,7 @@ class ProductionConfig(Config):
     @classmethod
     def init_app(cls, app):
         Config.init_app(app)
-
-        # email errors to the administrators
-        import logging
-        from logging.handlers import SMTPHandler
-        credentials = None
-        secure = None
-        if getattr(cls, 'MAIL_USERNAME', None) is not None:
-            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
-            if getattr(cls, 'MAIL_USE_TLS', None):
-                secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
-            fromaddr=cls.INCPL_MAIL_SENDER,
-            toaddrs=[cls.INCEPTION_ADMIN],
-            subject=cls.INCPL_MAIL_SUBJECT_PREFIX + ' Application Error',
-            credentials=credentials,
-            secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
-
+        
 
 config = {
     'development': DevelopmentConfig,
